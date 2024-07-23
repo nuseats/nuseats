77b5c4f1aaab110a5f2319bbae66bc7c5544753b
potaotototo <jingjingwang2004@outlook.com>
2024-07-23 22:13:05 +0800

    new profile screen

---
+++ client/app/Screens/ProfileScreen.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, TextInput, ScrollView, Image } from "react-native";
+import tw from 'twrnc';
+
+const ProfileScreen = () => {
+    return (
+        <Text> profile pic, username, past awards </Text>
+    );
+};
+
+export default ProfileScreen;
